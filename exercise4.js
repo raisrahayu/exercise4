3abbabeaf6214ef4be1e0618d426b9d6863f50a3
raisrahayu <rais.rahayu@gmail.com>
2023-07-29 07:33:32 +0700

    revisi

--- exercise4.js
+++ exercise4.js
@@ -1,7 +1,7 @@
 console.log("Exercise 4 Rais Rahayu")
-
+console.log("--------------------")
 // No.1
-console.log("No.1")
+console.log("No.1 Membuat Segitiga")
 function segitiga(tinggi) {
   const totalElements = (tinggi * (tinggi + 1)) / 2;
   const numDigits = String(totalElements).length;
@@ -22,7 +22,8 @@ segitiga(tinggi);
 
 
 // No.2
-console.log("No.2")
+console.log("--------------------")
+console.log("No.2 FizzBuzz")
 
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
@@ -43,8 +44,9 @@ function fizzBuzz(n) {
 // Penggunaan
 fizzBuzz(15);
 
-
-console.log("No.3")
+// No.3
+console.log("--------------------")
+console.log("No.3 Menghitung BMI")
 function hitungBMI(berat, tinggi1) {
   const tinggiMeter = tinggi1 / 100;
   const bmi = berat / (tinggiMeter * tinggiMeter);
@@ -68,7 +70,9 @@ const hasilBMI = hitungBMI(berat, tinggi1);
 console.log("kategori BMI:", hasilBMI);
 
 
-console.log("No.4")
+// No.4 
+console.log("--------------------")
+console.log("No.4 Menghilangkan Angka Ganjil")
 function hilangkanGanjil(arr) {
   const angkaGenap = arr.filter((num) => num % 2 === 0);
   return angkaGenap;
@@ -79,13 +83,14 @@ const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const hasilOddEven = hilangkanGanjil(inputArray);
 console.log(hasilOddEven);
   
-
-console.log("No.5")
-function stringKeArray(inputString) {
+// No.5
+console.log("--------------------")
+console.log("No.5 Mengubah String Ke Array")
+function stringKeArray(string) {
   return string.split(' ');
 }
 
 // Penggunaan
 const string = "Hello World";
-const resultArray = stringKeArray(string);
-console.log(resultArray);
+const hasilArray = stringKeArray(string);
+console.log(hasilArray);
